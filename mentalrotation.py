6ea7e1fd57355453c5c64ed290e3fc34546e0e77
mae.dagnogo <dagnogo.maelle@gmail.com>
2023-05-24 10:04:08 +0200

    third attempt

--- mentalrotation.py
+++ mentalrotation.py
@@ -1,7 +1,30 @@
 import random
 from expyriment import design, control, stimuli
+import numpy as np
 
-WAIT_TIME = 2000
+CONG_RESPONSE = 106 
+INCONG_RESPONSE = 102
+WAIT_TIME = 1000
+MAX_RESPONSE_DELAY = 4000
+
+A = (stimuli.Picture('shapeA.png'),"A")
+B = (stimuli.Picture('shapeB.png'),"B")
+C = (stimuli.Picture('shapeC.png'),"C")
+D = (stimuli.Picture('shapeD.png'),"D")
+E = (stimuli.Picture('shapeE.png'),"E")
+F = (stimuli.Picture('shapeF.png'),"F")
+G = (stimuli.Picture('shapeG.png'),"G")
+H = (stimuli.Picture('shapeH.png'),"H")
+I = (stimuli.Picture('shapeI.png'),"I")
+J = (stimuli.Picture('shapeJ.png'),"J")
+K = (stimuli.Picture('shapeK.png'),"K")
+L = (stimuli.Picture('shapeL.png'),"L")
+
+position1 = (-250,0)
+position2 = (250,0)
+
+shapes = [A, B, C, D, E, F, G, H, I, J, K, L]
+N_trials = len(shapes)
 
 exp = design.Experiment(name="Mental Rotation", text_size=40)
 #control.set_develop_mode(on=True)
@@ -10,61 +33,64 @@ control.initialize(exp)
 blankscreen = stimuli.BlankScreen()
 
 instructions = stimuli.TextScreen("Instructions",
-    f"""
-    There will be  trials in total.
+    f""" At each trial, you are gonna see to shapes presented next to each other. Your task is to indicate as fast as possible whether the two shapes are identical or not.
+
+    Press {CONG_RESPONSE} when you think they are identical and press {INCONG_RESPONSE} when you think they are not.
+
+    There will be {N_trials} trials in total.
 
     Press the spacebar to start.""")
 
 exp.add_data_variable_names(['trial', 'shape', 'congruence', 'rotation', 'respkey', 'RT'])
 
-A = [stimuli.Picture('shapeA1.png'),"A"]
-B = [stimuli.Picture('shapeB1.png'),"B"]
-C = [stimuli.Picture('shapeC1.png'),"C"]
-D = [stimuli.Picture('shapeD1.png'),"D"]
-E = [stimuli.Picture('shapeE1.png'),"E"]
 
-position1 = (-20,0)
-position2 = (250,0)
 
-shapes = [A, B, C, D, E]
-N_trials = len(shapes)
-random.shuffle(shapes)
+congruence = ['cong']*(int(N_trials/2))+(['incong']*(int(N_trials/2)))
+rotation_angles = [30,60,90,120,150,180]*2
+c = list(zip(congruence, rotation_angles))
+random.shuffle(c)
+congruence, rotation_angles = zip(*c)
 
-targets2 = []
-targets1 = []
-congruence = []
-rotation_angles = []
-
+block = design.Block("The one and only block")
 for i in range(N_trials):
     target1 = shapes[i][0]
-    n = random.randint(0,1)
-    rotation_angle = random.randint(0,180)
-    rotation_angles.append(rotation_angle)
 
-    if n == 0 :
-        target2=target1
-        congruence.append('cong')
-    elif n == 1 :
-        target2 = target1
+    if congruence[i] == 'cong' :
+        target2=target1.copy()
+        
+    elif congruence[i] == 'incong' :
+        target2=target1.copy()
         target2.flip((True,False))
-        congruence.append('incong')
   
     target1.reposition(position1)
-    targets1.append(target1)
     target2.reposition(position2)
-    target2.rotate(rotation_angle)
-    targets2.append(target2)
+    target2.rotate(rotation_angles[i])
+
+    trial = design.Trial()
+    trial.add_stimulus(target1)
+    trial.add_stimulus(target2)
+
+    trial.set_factor("rotation_angle", rotation_angles[i])
+    trial.set_factor("congruence", congruence[i])
+    trial.set_factor("shape", shapes[i][1])
+    block.add_trial(trial, copies=1)
+
+block.shuffle_trials()
 
 control.start(skip_ready_screen=True)
 instructions.present()
 exp.keyboard.wait()
 
-for j in range(N_trials):
-    targets2[j].plot(blankscreen)
-    targets1[j].plot(blankscreen)
+i=0
+for T in block.trials:
+    stim1, stim2 = T.stimuli
+    stim1.present(clear=True, update=False)
+    stim2.present(clear=False, update=True)
+    key, rt = exp.keyboard.wait([CONG_RESPONSE, INCONG_RESPONSE],
+                                     duration=MAX_RESPONSE_DELAY)
+    exp.data.add([i, T.get_factor("shape"), T.get_factor("congruence"), T.get_factor("rotation_angle"), key, rt])
     blankscreen.present()
-    key, rt = exp.keyboard.wait()
-    exp.data.add([j, shapes[j][1], congruence[j], rotation_angles[j], key, rt])
-
+    exp.clock.wait(WAIT_TIME)
+    i = i+1
 
 control.end()
